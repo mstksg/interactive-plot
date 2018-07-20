f4ab1cfe84c787d20a52afd710371257eed8f77c
Justin Le <justin@jle.im>
2018-07-19 17:16:14 -0700

    Update gh-pages at Thu Jul 19 17:16:14 PDT 2018

--- CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-# Changelog for interactive-plot
-
-## Unreleased changes
--- app/demo.hs
+++
@@ -1,9 +0,0 @@
-import           Data.Default
-import           Interactive.Plot
-
-cosTest  = funcSeries cos (enumRange 100 (R (-5) 5)) def
-lineTest = funcSeries id  (enumRange 20  (R (-4) 4)) def
-
-main :: IO ()
-main = runPlotAuto def [cosTest, lineTest]
-
