348921d7de294d253212813f62a970f5f9eb9ca3
Justin Le <justin@jle.im>
2018-07-19 18:11:37 -0700

    type sgnatures in demo to appease compiler

--- app/demo.hs
+++ app/demo.hs
@@ -1,6 +1,7 @@
 import           Data.Default
 import           Interactive.Plot
 
+cosTest, lineTest :: AutoSeries
 cosTest  = funcSeries cos (enumRange 100 (R (-5) 5)) def
 lineTest = funcSeries id  (enumRange 20  (R (-4) 4)) def
 
